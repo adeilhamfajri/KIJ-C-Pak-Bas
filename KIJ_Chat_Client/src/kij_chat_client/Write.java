2997deb17cd9cb39bc85dbab2d19787f79cfc73a
dewi2004 <dewi.12347.9f@gmail.com>
2016-04-27 12:09:43 +0700

    Added files via upload
    penambahan fungsi untuk generate key dan mengenkripsi key

--- KIJ_Chat_Client/src/kij_chat_client/Write.java
+++ KIJ_Chat_Client/src/kij_chat_client/Write.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package kij_chat_client;
 
 import java.io.PrintWriter;
@@ -22,7 +17,6 @@ import javax.crypto.IllegalBlockSizeException;
 import javax.crypto.NoSuchPaddingException;
 import sun.misc.BASE64Encoder;
 /**
-
 /**
  *
  * @author santen-suru
@@ -46,6 +40,8 @@ public class Write implements Runnable {
     private PrintWriter out;
     boolean keepGoing = true;
     ArrayList<String> log;
+    private Object AES;
+    private String encryptedMsg;
 	
     public Write(Scanner chat, PrintWriter out, ArrayList<String> log)
     {
@@ -97,6 +93,16 @@ public class Write implements Runnable {
                            out.println(input);//SEND IT TO THE SERVER
 
                        }
+                        
+                        
+                        String algorithm = "AES";           //tambahan
+                                
+                        privateKey = newSecretKeySpec(keyValue, algorithm);     //tambahan
+                        privateKey = generateKey();             //tambahan
+                        encryptedMsg = AES.encrypt(plaintext);          //tambahan
+                                
+                        out.println("Enkripsi = " + encryptedMsg);      //tambahan
+                        
                         out.flush(); //flush stream-nya
                         
                        if (input.contains("logout")) {
@@ -111,5 +117,9 @@ public class Write implements Runnable {
 			e.printStackTrace();//MOST LIKELY WONT BE AN ERROR, GOOD PRACTICE TO CATCH THOUGH
 		} 
 	}
+
+    private Key generateKey() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
 
 }
